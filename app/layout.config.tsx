87f7c6d86623396551db4eac313c2dc0048e5adf
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2025-03-12 23:03:56 +0100

    Update layout.config.tsx

--- app/layout.config.tsx
+++ app/layout.config.tsx
@@ -1,9 +1,13 @@
+import clsx from 'clsx';
 import type { BaseLayoutProps } from 'fumadocs-ui/layouts/shared';
+import { Oswald } from 'next/font/google';
+
+const oswald = Oswald({ subsets: ['latin'], display: 'swap' });
 
 /**
  * Shared layout configurations
  *
- * you can customise layouts individually from:
+ * you can customize layouts individually from:
  * Home Layout: app/(home)/layout.tsx
  * Docs Layout: app/docs/layout.tsx
  */
@@ -11,15 +15,7 @@ export const baseOptions: BaseLayoutProps = {
   nav: {
     title: (
       <>
-        <svg
-          width="24"
-          height="24"
-          xmlns="http://www.w3.org/2000/svg"
-          aria-label="Logo"
-        >
-          <circle cx={12} cy={12} r={12} fill="currentColor" />
-        </svg>
-        My App
+        <span className={clsx(oswald.className, 'text-2xl')}>CRAIG SCOTT</span>
       </>
     ),
   },
