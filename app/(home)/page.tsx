5aaa1d459aa65246712cd221f7d9495de7d533b8
Craig Scott <107496589+craigscottuk@users.noreply.github.com>
2025-03-13 12:44:53 +0100

    Create Design, UI & state 04 selected answer diagram.svg

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -2,17 +2,28 @@ import Link from 'next/link';
 
 export default function HomePage() {
   return (
-    <main className="flex flex-1 flex-col justify-center text-center">
-      <h1 className="mb-4 text-2xl font-bold">Hello World</h1>
-      <p className="text-fd-muted-foreground">
-        You can open{' '}
+    <main className='flex flex-1 flex-col justify-center text-center text-pretty max-w-[50ch] mx-auto'>
+      <h1 className='mb-4 text-2xl font-bold'>
+        Hello, and thank you for visiting!
+      </h1>
+      <p className='text-fd-muted-foreground text-pretty'>
+        This site showcases my technical writing skills, demonstrating my
+        ability to structure and present clear, effective documentation.
+      </p>
+      <p className='text-fd-muted-foreground text-pretty mt-4'>
+        Explore the{' '}
         <Link
-          href="/docs"
-          className="text-fd-foreground font-semibold underline"
+          href='/docs'
+          className='text-fd-foreground font-semibold underline'
         >
           /docs
         </Link>{' '}
-        and see the documentation.
+        section for a sample of my work.
+      </p>
+      <p className='text-fd-muted-foreground text-pretty mt-4'>
+        I am keen to contribute to <span className='font-bold'>Antmicro’s</span>{' '}
+        work in open-source computing. If you have any questions or would like
+        to discuss my experience further, please get in touch.
       </p>
     </main>
   );
